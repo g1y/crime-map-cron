66852a6fcc4d213ecf477ad9eba78affd7af6502
Guy Moore <guy.moore.normal@gmail.com>
2018-03-11 20:06:06 -0700

    Add tests for the slopd log parsing

---
+++ tests/test_parse_slopd_logs.py
@@ -0,0 +1,22 @@
+import unittest
+import slopd_log_parse
+
+class TestParseSLOPDLogs(unittest.TestCase):
+	def test_parse_log_address(self):
+		sample_file = open('sample_slopd_log.txt', 'r')
+		sample_log = sample_file.read()
+		parsed = slopd_log_parse.parse_log(sample_log)
+		for entry in parsed:
+				self.assertIn('address', entry)
+		sample_file.close()
+
+	def test_parse_date_and_time(self):
+		entry = self.get_first_entry()
+		print(entry["date"])
+		self.assertEqual('09/07/17', entry["date"])
+
+	def get_first_entry(self):
+		sample_file = open('sample_slopd_log.txt', 'r')
+		sample_log = sample_file.read()
+		parsed = slopd_log_parse.parse_log(sample_log)
+		return parsed[0]
